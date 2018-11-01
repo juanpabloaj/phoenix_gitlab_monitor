90dc8b763e2f35381790fc8567b7c56638426408
JuanPablo <jpabloaj@gmail.com>
2018-11-01 16:37:23 -0300

    pipeline info is sending to channel

--- lib/monitor_web/controllers/api_controller.ex
+++ lib/monitor_web/controllers/api_controller.ex
@@ -1,9 +1,28 @@
 defmodule MonitorWeb.ApiController do
   use MonitorWeb, :controller
 
+  plug :fetch_pipeline
+
+  def pipeline_info(params) do
+    %{
+      object_attributes: params["object_attributes"],
+      project: params["project"],
+      commit: params["commit"]
+    }
+  end
+
+  def fetch_pipeline(conn, _) do
+    case conn |> get_req_header("x-gitlab-event") do
+      [] -> conn
+      ["Pipeline Hook"] ->
+        MonitorWeb.Endpoint.broadcast! "room:lobby",
+          "new_msg",
+          %{body: pipeline_info(conn.params)}
+        conn
+    end
+  end
+
   def index(conn, _params) do
-    MonitorWeb.Endpoint.broadcast! "room:lobby",
-      "new_msg", %{body: "hello from API..."}
-    json conn, %{message: "hello world"}
+    json conn, %{message: "info received ok"}
   end
 end
